6383b7053c36949003213aa2c0519678a2d13b3e
leonelsils <leonelads7@gmail.com>
2017-07-29 15:35:37 -0300

    Adicionando alterações no cadastro de produtos

--- fisiomais/app/controllers/products_controller.rb
+++ fisiomais/app/controllers/products_controller.rb
@@ -8,4 +8,39 @@ class ProductsController < ApplicationController
 	def new
 		@product = Product.new
 	end
+
+	def new
+		@product = Product.new
+	end
+
+	def create
+		@product = Product.new(product_params)
+
+		if @product.save
+			redirect_to products_path, notice: "Produto cadastrado com sucesso!"
+		else
+			render :new
+		end
+	end
+
+	def edit
+	end
+
+	def update
+		@product.assign_attributes(product_params)
+
+		if @product.save
+			redirect_to products_path, notice: "Produto foi Atualizado com sucesso"
+		else
+			render :edit
+		end
+	end
+
+	def set_product
+		@product = Product.find(params[:id])
+	end
+
+	def product_params
+		params.require(:product).permit(:name, :price, :status)
+	end
 end
--- fisiomais/config/routes.rb
+++ fisiomais/config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  #Products of routes
-   get "products" => 'products#index', as: :products
-   get 'products/new' => 'products#new', as: :new_product
+  	#Products of routes
+  	get 	'products' 				=> 'products#index', 	as: :products
+  	post 	'products' 				=> 'products#create'
+  	get 	'products/new' 			=> 'products#new', 		as: :new_product
+  	get 	'product/:id/edit'	 	=> 'products#edit', 	as: :edit_product
+  	get 	'author/:id' 			=> 'products#show',		as: :product
+	patch 	'product/:id' 			=> 'products#update'
 
-  root :to => "welcome#index"
+  	root :to => 'welcome#index'
 end
